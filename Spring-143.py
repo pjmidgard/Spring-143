b2c4b7da85f053e8907f57db97e4bb90a2a89eff
Java Software <pj.midgard@yahoo.com>
2022-10-15 14:05:08 +0100

    Update Spring-143.py

--- Spring-143.py
+++ Spring-143.py
@@ -1,3 +1,9 @@
+#Spring-143
+
+#Algorithm Spring-143:
+
+#We should take 64 bits and find two the same #variations for 32-64 bits long and 0-64 bit, then, #save where it is a 3 bits variation, 8 combination #and 4 bits variation, 16 combination. Add one bit #next and try to find again if not find that block it's #compress and find variation should be random.
+
 from time import time
 cvf=0
 import os
@@ -337,7 +343,7 @@ class encypthion_class:
                                                                    after_block=Calculus
                                                                               
                                                                                  
-                                                                elif limit_find==0 and l==64 and res_one_time==0 and times_compress<201:
+                                                                elif limit_find==0 and l==64 and res_one_time==0:
                                                                    after_block=res64
                                                                    res_one_time=1
                                                                    block+=1
